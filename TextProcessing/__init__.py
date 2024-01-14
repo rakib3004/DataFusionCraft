7f36c6f2ac56cb4dc10477b164f795389534deba
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-01-14 15:29:33 +0600

    remove absulate path instead of relative path in text processing

--- TextProcessing/__init__.py
+++ TextProcessing/__init__.py
@@ -1,20 +1,15 @@
-#C:\Program Files\Tesseract-OCR
 from PIL import Image
 import pytesseract
 
-# Path to the Tesseract executable (update it based on your installation)
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\pc\Documents\Github\DataFusionCraft\TextProcessing\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
 
 def extract_bangla_text_from_image(image_path):
-    # Open the image using PIL (Python Imaging Library)
     image = Image.open(image_path)
 
-    # Use pytesseract to do OCR on the image with the Bengali language
     text = pytesseract.image_to_string(image, lang='ben')
 
     return text
 
-# Example usage
 image_path = "page_180.png"
 extracted_bangla_text = extract_bangla_text_from_image(image_path)
 
